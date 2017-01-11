730bf9371943f56e60e993dad38a54c72737981f
DESKTOP-CA94RL6\hiran <hiran123@live.com>
2017-01-11 13:24:49 +0530

    C

--- app/config/database.js
+++ app/config/database.js
@@ -1,4 +1,4 @@
 module.exports= {
     'secret': 'TaileredFitSecret',
-    'database': 'mongodb://ec2-54-218-176-105.us-west-2.compute.amazonaws.com:27017/TaileredFit'
+    'database': 'mongodb://ec2-54-218-176-105.us-west-2.compute.amazonaws.com/TaileredFit'
 };
