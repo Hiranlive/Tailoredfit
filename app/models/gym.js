11e334d5950ba3d2ecd2a6d2a35c3ae6e7330e7a
DESKTOP-CA94RL6\hiran <hiran123@live.com>
2017-01-12 15:32:41 +0530

    sdsd

--- app/models/gym.js
+++ app/models/gym.js
@@ -7,7 +7,7 @@ var gymSchema = mongoose.Schema({
 		type : String,
 		require : true
 	},
-	type : {
+	gym_type : {
 		type : String,
 		require : true
 	},
@@ -71,7 +71,7 @@ module.exports.updateGym = function(id, gym_item, options, callback) {
 	
 	var update = {
 		name : gym_item.name,
-		type : gym_item.type,
+		type : gym_item.gym_type,
 		address : gym_item.address,
 		latitude : gym_item.latitude,
 		longitude : gym_item.longitude,
