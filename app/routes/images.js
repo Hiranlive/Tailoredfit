7a95f48dee097e4407def71245037978ebdb0f4f
DESKTOP-CA94RL6\hiran <hiran123@live.com>
2017-02-07 13:38:43 +0530

    wewff

---
+++ app/routes/images.js
@@ -0,0 +1,28 @@
+var express  = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.post('/api/upload', function function_name(req, res) {
+	console.log(req.files.image.originalFilename);
+	console.log(req.files.image.path);
+	fs.readFile(req.files.image.path, function (err, data){
+		var newPath = "../../uploads/" + req.files.image.originalFilename;
+		fs.writeFile(newPath, data, function (err) {
+			if(err){
+				res.json({'response':"Error"+err});
+			}else {
+				res.json({'response':"Saved"});		  
+			}
+		});
+	});
+});
+
+router.get('/api/upload/:file', function function_name(req, res) {
+	file = req.params.file;
+	var img = fs.readFileSync("../../uploads/" + file);
+	res.writeHead(200, {'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
+});
+
+
+module.exports = router;
